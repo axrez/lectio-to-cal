a7966cc50c9d0ecc5ceca901c34f31acf8146106
Emil <isdragon112@gmail.com>
2019-01-17 21:17:45 +0100

    Started and tested stuff

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const cheerio = require('cheerio')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => res.json({ msg: 'diller' }))
+
+app.get('/test', (req, res) => {
+  const options = {
+    url: 'https://www.lectio.dk/lectio/165/SkemaNy.aspx?type=elev&elevid=26559177918&week=032019'
+  }
+  request(options, (error, response, body) => {
+    const $ = cheerio.load(body)
+    console.log($('.s2skema'))
+  })
+})
+
+const port = process.env.PORT || 3001
+app.listen((port), () => console.log(`Server is listening on port ${port}`))
