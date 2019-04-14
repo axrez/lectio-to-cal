6e9f17a09515bb844e3301db3c3895eb0605e672
axrez <isdragon112@gmail.com>
2019-04-14 17:45:13 +0200

    meh

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const cheerio = require('cheerio')
+import { getHTML, twCount } from './lib/fetch'
 
-const app = express()
+const go = async () => {
+  const html = await getHTML();
+  const twitterFollowers = await twCount(html);
+  console.log(twitterFollowers);
+}
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+go()
 
-app.get('/', (req, res) => res.json({ msg: 'diller' }))
-
-app.get('/test', (req, res) => {
-  const options = {
-    url: 'https://www.lectio.dk/lectio/165/SkemaNy.aspx?type=elev&elevid=26559177918&week=032019'
-  }
-  request(options, (error, response, body) => {
-    const html = body.match(/(<body[^]*<\/body>)/)[0]
-    const $ = cheerio.load(html)
-    res.send($('*').html())
-  })
-})
-
-const port = process.env.PORT || 3001
-app.listen((port), () => console.log(`Server is listening on port ${port}`))
