08ee265157629fb6e9ea25f9745f7c172e1f7a74
BuildTools <unconfigured@null.spigotmc.org>
2019-04-15 16:50:35 +0200

    Got a method for getting the authtoken working

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-import { getHTML, twCount } from './lib/fetch'
+// import { getHTML, twCount } from './lib/scrape'
+import { getAuthToken } from './lib/lectioAuth';
+
+import { cred } from './config/lectioCred';
 
 const go = async () => {
-  const html = await getHTML();
-  const twitterFollowers = await twCount(html);
-  console.log(twitterFollowers);
-}
+  const token = await getAuthToken(cred.username, cred.password);
+  console.log(token);
+};
 
-go()
-
+go();
